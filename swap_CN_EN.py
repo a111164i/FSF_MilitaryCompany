50575e1a99f9ec2547a53d63b6bee36d096cb1dd
jpei5708 <jpei5708@uni.sydney.edu.au>
2023-12-04 01:15:26 +1100

    4.2rc7

--- swap_CN_EN.py
+++ swap_CN_EN.py
@@ -27,6 +27,7 @@ def swap_file_csv(file_path: str, file_name_without_extension: str, swap_fields:
 
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
+            print(f'Swap Load {file_name_without_extension}')
             csv_reader = csv.DictReader(file)    
             # row is List[Dict[str, str]]
             for row in csv_reader:
@@ -44,6 +45,9 @@ def swap_file_csv(file_path: str, file_name_without_extension: str, swap_fields:
                 csv_reader = csv.DictReader(file)    
                 for row in csv_reader:
                     diction_rows_other.append(row)  
+        else:
+            print('Failed to load csv')
+            return
 
     # find common ids  between two diction list
     common_ids = set(row['id'] for row in diction_rows_now).intersection(row['id'] for row in diction_rows_other)
@@ -78,50 +82,62 @@ def swap_file_csv(file_path: str, file_name_without_extension: str, swap_fields:
         
     # Save the swaped diction_rows_now to the original CSV file
     with open(file_path, 'w', encoding='utf-8', newline='') as file:
-        fieldnames = diction_rows_now[0].keys()
-        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
-        csv_writer.writeheader()
+        try:
+            fieldnames = diction_rows_now[0].keys()
+            csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+            csv_writer.writeheader()
 
-        for row in diction_rows_now:
-            try:
-                csv_writer.writerow(row)
-            except e:
-                print(f"Error write csv_to_use")
-                continue
-                
+            for row in diction_rows_now:
+                try:
+                    csv_writer.writerow(row)
+                except Exception as e:
+                    print(f"Error write row{row} csv_to_now")
+                    continue
+        except Exception as e:
+            print("Fail to Write now csv")      
+            return
+         
     # Save the swaped diction_rows_other to a new CSV file
     # EN_to_CN means load _CN, the old data save to _EN as backup 
     # if load _CN, save old data to _EN 
     if EN_to_CN:
         with open(file_path_EN, 'w', encoding='utf-8', newline='') as file:
-            fieldnames_other = diction_rows_other[0].keys()
-            csv_writer_other = csv.DictWriter(file, fieldnames=fieldnames_other)
-            csv_writer_other.writeheader()
+            try:
+                fieldnames_other = diction_rows_other[0].keys()
+                csv_writer_other = csv.DictWriter(file, fieldnames=fieldnames_other)
+                print(f'Swap Write {file_name_without_extension}')
+                csv_writer_other.writeheader()
 
-            for row_other in diction_rows_other:
-                try:
-                    csv_writer_other.writerow(row_other)
-                except e:
-                    print(f"Error write a  row csv_to_backup")
-                    continue
+                for row_other in diction_rows_other:
+                    try:
+                        csv_writer_other.writerow(row_other)
+                    except Exception as e:
+                        print(f"Error write row{row_other} csv_to_backup")
+                        continue
+            except Exception as e:
+                print("Fail to Write other csv")
         os.remove(file_path_CN)
     else:
         with open(file_path_CN, 'w', encoding='utf-8', newline='') as file:
-            fieldnames_other = diction_rows_other[0].keys()
-            csv_writer_other = csv.DictWriter(file, fieldnames=fieldnames_other)
-            csv_writer_other.writeheader()
+            try:
+                fieldnames_other = diction_rows_other[0].keys()
+                csv_writer_other = csv.DictWriter(file, fieldnames=fieldnames_other)
+                csv_writer_other.writeheader()
+                print(f'Swap Write {file_name_without_extension}')
 
-            for row_other in diction_rows_other:
-                try:
-                    csv_writer_other.writerow(row_other)
-                except e:
-                    print(f"Error write a row csv_to_backup")
-                    continue
-                
+                for row_other in diction_rows_other:
+                    try:
+                        csv_writer_other.writerow(row_other)
+                    except Exception as e:
+                        print(f"Error write row{row_other} csv_to_backup")
+                        continue
+            except Exception as e:
+                print("Fail to Write other csv")
         os.remove(file_path_EN)
+    print(f'Swap Done {file_name_without_extension}')
 
 if __name__ == "__main__":
     swap_file_csv("data/campaign/submarkets.csv", "submarkets", ['name', 'desc'])
-    swap_file_csv("data/campaign/rules.csv", "rules", ['script','text','options'])
+    #swap_file_csv("data/campaign/rules.csv", "rules", ['script','text','options'])
     swap_file_csv("data/campaign/industries.csv", "industries", ['name','desc'])
     swap_file_csv("data/strings/descriptions.csv", "descriptions", ['text1','text2','text3','text4'])
